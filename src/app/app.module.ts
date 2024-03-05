e52f473ed06a9423a6864ecebcdcbd610e3bef70
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2024-03-04 19:04:15 -0600

    working on routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { LeftBarComponent } from './left-bar/left-bar.component';
+import { HomeTaskComponent } from './home-task/home-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    LeftBarComponent,
+    HomeTaskComponent
   ],
   imports: [
     BrowserModule,
